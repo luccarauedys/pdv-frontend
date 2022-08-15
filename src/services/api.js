25cc20a78890c6d56180abcbe8c4bff091665690
Lucca Rauédys <luccarauedys@outlook.com>
2022-08-15 17:20:18 -0300

    fix: config da requisicao

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://projeto-pdv.herokuapp.com/",
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+  // headers: {
+  //   Authorization: `Bearer ${localStorage.getItem("token")}`,
+  // },
 });
+
+const getConfig = () => {
+  const token = localStorage.getItem("token");
+  const config = { headers: { Authorization: token } };
+  return config;
+};
 
 /* Company */
 
@@ -18,61 +24,61 @@ export const signIn = async (signInData) => {
 };
 
 export const getCompanyData = async () => {
-  return await api.get("/companies");
+  return await api.get("/companies", getConfig());
 };
 
 /* Products */
 
 export const createProduct = async (productData) => {
-  return await api.post("/products", productData);
+  return await api.post("/products", productData, getConfig());
 };
 
 export const getProducts = async () => {
-  return await api.get("/products");
+  return await api.get("/products", getConfig());
 };
 
 export const getProductsByName = async (productName) => {
-  return await api.get(`/products/?name=${productName}`);
+  return await api.get(`/products/?name=${productName}`, getConfig());
 };
 
 export const updateProduct = async (productData) => {
   const { id } = productData;
-  return await api.put(`/products/${id}`, productData);
+  return await api.put(`/products/${id}`, productData, getConfig());
 };
 
 export const deleteProduct = async (productId) => {
-  return await api.delete(`/products/${productId}`);
+  return await api.delete(`/products/${productId}`, getConfig());
 };
 
 /* Sales */
 
 export const createSale = async (saleData) => {
-  return await api.post("/sales", saleData);
+  return await api.post("/sales", saleData, getConfig());
 };
 
 export const getSales = async () => {
-  return await api.get("/sales");
+  return await api.get("/sales", getConfig());
 };
 
 export const deleteSale = async (saleId) => {
-  return await api.delete(`/sales/${saleId}`);
+  return await api.delete(`/sales/${saleId}`, getConfig());
 };
 
 /* Expenses */
 
 export const createExpense = async (expenseData) => {
-  return await api.post("/expenses", expenseData);
+  return await api.post("/expenses", expenseData, getConfig());
 };
 
 export const getExpenses = async () => {
-  return await api.get("/expenses");
+  return await api.get("/expenses", getConfig());
 };
 
 export const updateExpense = async (expenseData) => {
   const expenseId = expenseData.id.toString();
-  return await api.put(`/expenses/${expenseId}`, expenseData);
+  return await api.put(`/expenses/${expenseId}`, expenseData, getConfig());
 };
 
 export const deleteExpense = async (expenseId) => {
-  return await api.delete(`/expenses/${expenseId}`);
+  return await api.delete(`/expenses/${expenseId}`, getConfig());
 };
